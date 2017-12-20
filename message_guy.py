fd6ef658003d00a10a39d7f9ba0d70b49317dd0e
Leonardo Antunes <antunesleo4@gmail.com>
2017-12-20 21:32:08 -0200

    Script to send error to logstash

---
+++ message_guy.py
@@ -0,0 +1,25 @@
+import socket
+import json
+import sys
+
+HOST = '0.0.0.0'
+PORT = 9201
+
+try:
+  sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+except socket.error as ex:
+  sys.stderr.write("[ERROR] {}".format(ex.message))
+  sys.exit(1)
+
+try:
+  sock.connect((HOST, PORT))
+except socket.error, msg:
+  sys.stderr.write("[ERROR] %s\n" % msg[1])
+  sys.exit(2)
+
+error_message = {'@message': 'A beautiful and cool error message', '@tags': ['test']}
+
+sock.send(json.dumps(error_message) + "\n")
+
+sock.close()
+sys.exit(0)
